bbe9d6efc8b07c7ef06bd094a1d5af36b1421eb5
melak-12 <melakabebeee@gmail.com>
2023-12-06 12:47:33 +0300

    serv

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 dotenv.config()
 connectDb();
 const app = express();
-app.use(cors({ origin: 'http://localhost:3001'||'https://restaurants-blond-five.vercel.app' }));
+app.use(cors({ origin:'https://restaurants-blond-five.vercel.app' }));
 // this are midlewares to use body and json 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,7 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/foods', require('./routes/foodRoutes'));
 app.use('/api/menus', require('./routes/menuRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
-//server r 
 app.get('/', (req, res) => {
     res.send("welcome angel")
 });
