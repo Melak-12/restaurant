df8c9b892e7175826cf1776c0ed6944e994d7964
melak-12 <melakabebeee@gmail.com>
2024-02-22 16:17:53 +0300

    updating time

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,16 +1,19 @@
-"use client"
-import React from 'react'
-import Countdown from 'react-countdown'
+"use client";
+import React from "react";
+import Countdown from "react-countdown";
 
-const endingDate = new Date("2023-12-14")
+const endingDate = new Date("2024-12-14");
 
 const CountDown = () => {
   return (
-    <Countdown className='font-bold text-lg md:text-5xl text-yellow-300' date={endingDate}/>
+    <Countdown
+      className='font-bold text-lg md:text-5xl text-yellow-300'
+      date={endingDate}
+    />
     // <span>new day </span>
     // <span>new day </span>
     // <span>new day </span>
-  )
-}
+  );
+};
 
-export default CountDown
+export default CountDown;
--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -6,26 +6,30 @@ import Link from "next/link";
 const Offer = () => {
   return (
     <div className="bg-slate-800 h-30 mt-16 flex flex-row md:flex-row md:justify-between md:bg-[url('/bg.png')] md:h-[50vh]">
-      <div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
-        <h1 className="text-white text-lg font-bold xl:text-6xl  md:text-5xl">Delicious Burger</h1>
-        <p className="text-white xl:text-xl text-sm">
+      <div className='flex-1 flex flex-col justify-center items-center text-center gap-8 p-6'>
+        <h1 className='text-white text-lg font-bold xl:text-6xl  md:text-5xl'>
+          Delicious Burger
+        </h1>
+        <p className='text-white xl:text-xl text-sm'>
           Progressively simplify effective e-toilers and process-centric methods
-          
         </p>
-        <p className="hidden md:block text-white">of empowerment. Quickly pontificate parallel.</p>
-        <CountDown/>
-       <Link href='/product/6552626b18b4dff3c8f747f3'>
-        <button className="bg-green-500 text-white rounded-md py-3 px-6">Order Now</button>
-       </Link>
+        <p className='hidden md:block text-white'>
+          of empowerment. Quickly pontificate parallel.
+        </p>
+        <CountDown />
+        <Link href='/product/6552626b18b4dff3c8f747f3'>
+          <button className='bg-green-500 text-white rounded-md py-3 px-6'>
+            Order Now
+          </button>
+        </Link>
       </div>
       {/* <div className="flex-1">
       <Image src="/p2.png" alt="" width={110} height={110} className="object-contain" />
       </div> */}
       <span></span>
-      <div className="flex-1 w-full relative md:h-full">
-        <Image src="/p2.png" alt="" fill className="object-contain" />
+      <div className='flex-1 w-full relative md:h-full'>
+        <Image src='/p2.png' alt='' fill className='object-contain' />
       </div>
-
     </div>
   );
 };
